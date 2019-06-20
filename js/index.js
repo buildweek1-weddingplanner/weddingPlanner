189575b4af1b4d3d47014fccf8d311def6f6ff1a
Jason Loomis <jason@jrloomis.com>
2019-06-19 23:05:30 -0400

    refactored scroll effects, moved nav link hover effect to css

--- js/index.js
+++ js/index.js
@@ -1,36 +1,33 @@
-// Goal: Refactor the class to include headerTransparency and headerTitleColor - one class to rule them all...
-//       Remove the hover effects - let CSS handle them
+class ScrollEffects {
+  constructor(element) {
+    this.element = element;
+    this.headerBackground = this.element.querySelector(".header");
+    this.titleColor = this.headerBackground.querySelector(".header__title--link");
+    this.linkNav = this.element.querySelectorAll(`.header__link[data-tab='linkNav']`).forEach(link => new NavLink(link));
+    window.addEventListener("scroll", () => this.scrollEffects());
+  }
+  scrollEffects() {
+    if (window.scrollY >= 200) {
+      this.headerBackground.style.backgroundColor = "#5e81ac";
+      this.headerBackground.style.transition = "background-color 500ms";
+      this.titleColor.style.color = "#e5e9f0";
+      this.titleColor.style.transition = "color 300ms";
+    } else {
+      this.headerBackground.style.backgroundColor = "transparent";
+      this.headerBackground.style.transition = "background-color 500ms";
+      this.titleColor.style.color = "#2e3440";
+    }
+  }
+}
 
 class NavLink {
   constructor(element) {
     this.element = element;
-    this.element.addEventListener("mouseover", () => this.mouseOver());
-    this.element.addEventListener("mouseout", () => this.mouseOut());
-    window.addEventListener("scroll", () => this.changeLinkColor());
+    window.addEventListener("scroll", () => this.scrollEffects());
   }
-  mouseOver() {
-    this.element.style.borderBottom = "2px solid rgba(76, 86, 106, 0.8)";
-  }
-  mouseOut() {
-    this.element.style.borderBottom = "none";
-  }
-  changeLinkColor() {
+  scrollEffects() {
     window.scrollY >= 200 ? (this.element.style.color = "#d8dee9") : (this.element.style.color = "#2e3440");
   }
 }
 
-const linkNav = document.querySelectorAll(`.header__link[data-tab='linkNav']`).forEach(link => new NavLink(link));
-
-const headerTransparency = document.querySelector(".header");
-const headerTitleColor = document.querySelector(".header__title--link");
-window.addEventListener("scroll", () => {
-  if (window.scrollY >= 200 /* change to suit */) {
-    headerTransparency.style.backgroundColor = "#5e81ac"; // change color
-    headerTransparency.style.transition = "background-color 500ms";
-    headerTitleColor.style.color = "white";
-    headerTitleColor.style.transition = "color 500ms";
-  } else {
-    headerTransparency.style.backgroundColor = "transparent";
-    headerTitleColor.style.color = "#2e3440";
-  }
-});
+const scroll = document.querySelectorAll(".container-page").forEach(scroll => new ScrollEffects(scroll));
